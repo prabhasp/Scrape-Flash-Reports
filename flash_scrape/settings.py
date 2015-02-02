5fc5eee5b44b001d9cac20b98016eae0a9408fda
prabhasp <prabhas.pokharel@gmail.com>
2015-02-02 22:55:17 +0545

    Initial commit

---
+++ flash_scrape/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for flash_scrape project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'flash_scrape'
+
+SPIDER_MODULES = ['flash_scrape.spiders']
+NEWSPIDER_MODULE = 'flash_scrape.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'flash_scrape (+http://www.yourdomain.com)'
