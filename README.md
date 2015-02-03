84e6650b4b3f8c21d499e629654d48f50700b96e
prabhasp <prabhas.pokharel@gmail.com>
2015-02-03 11:56:57 +0545

    Adding schools spider + instructions on how to use. Yet to tests school scraping.

--- README.md
+++ README.md
@@ -1,10 +1,39 @@
-## Scraping Flash Education Reports
+## Scraping Flash School Reports
+This project is about scraping the Flash School Reports in Nepal to create an
+easily machine-readable dataset about the schools in Nepal.
 
-### How to get the data yourself
-The following are the commands to download the `data` yourself:
+It is also partially a learning project to learn the [scrapy](http://scrapy.org)
+scraping platform.
 
+
+### Getting started
+ * Install [scrapy](http://doc.scrapy.org/en/latest/intro/install.html#intro-install)
+ * Optional: Follow the [scrapy Intro tutorial](http://doc.scrapy.org/en/latest/intro/tutorial.html)
+
+Get the data! After cloning this repository and cd-ing into the correct directory,
 ```
+cd scrape_flash_reports
 mkdir data
-scrapy crawl DistrictLister -o data/districts.csv
-scrapy crawl VDCLister -o data/vdcs.csv
 ```
+To get the districts / vdc lists, you can run:
+```
+scrapy crawl districts -o data/districts.csv
+scrapy crawl vdcs -o data/vdcs.csv
+```
+
+To test that each school is being parsed properly, try:
+```
+scrapy parse 'http://202.70.77.75:8080/flash/schoolreport/reportshow.php?d=69&v=002&s=690020002&t=&yr=2070' --spider=schools --callback=parse_school_page
+```
+
+To scrape all the schools (untested), you can run:
+```
+scrapy crawl schools -o data/schools.csv -s JOBDIR=crawls/schools-1
+```
+This crawl can be stopped using CTRL-C and re-started, and should re-start from where it was. This is untested, but the documentation is from [here](http://doc.scrapy.org/en/latest/topics/jobs.html)
+
+### Contributors
+Contributors to this project:
+ * Bikram Adhikari (@meadhikari)
+ * Sakar Pudasaini (@karkhana)
+ * Prabhas Pokharel (@prabhasp)
---
+++ scrape_flash_reports/spiders/schools.py
@@ -0,0 +1,81 @@
+import scrapy
+from scrape_flash_reports.items import District, VDC, School
+
+BASE_URL = 'http://202.70.77.75:8080/flash/schoolreport'
+BASE_LIST_URL = BASE_URL + '/reportprebe.php'
+BASE_SHOW_URL = BASE_URL + '/reportshow.php'
+YEARS = range(2064, 2071)
+
+def val(sel):
+    return sel.extract()[0].strip()
+
+class SchoolistSpider(scrapy.Spider):
+    name = "schools"
+    allowed_domains = ["202.70.77.75"]
+    start_urls = [BASE_LIST_URL + "?req=distlist"]
+
+    def parse(self, response):
+        options = response.css('option')
+        # The first option is -Select District-, so we ignore it
+        # and only use [1:] of the options
+        for o in options[1:]:
+            district = District()
+            district['name'] = o.xpath('text()').extract()
+            district['code'] = o.xpath('@value').extract()
+            vdc_list_url = BASE_LIST_URL + '?req=vdclist&distcode=' + district['code'][0]
+            request = scrapy.Request(vdc_list_url, callback=self.parse_vdc)
+            request.meta['district'] = dict(district)
+            yield request
+
+    def parse_vdc(self, response):
+        options = response.css('option')
+        # The first option is -Select VDC-, so we ignore it
+        # and only use [1:] of the options
+        for o in options[1:]:
+            district = response.meta['district']
+            vdc = VDC()
+            vdc['name'] = o.xpath('text()').extract()
+            vdc['code'] = o.xpath('@value').extract()
+            vdc['district_code'] = district['code']
+            vdc['district_name'] = district['name']
+            for y in YEARS:
+                school_list_url = BASE_LIST_URL + '?req=schoollist&distcode=' + district['code'][0] + \
+                                  '&vdccode=' + vdc['code'][0] + '&y=2070'
+            request = scrapy.Request(school_list_url, callback=self.parse_school_list)
+            request.meta['vdc'] = dict(vdc)
+            yield request
+
+    def parse_school_list(self, response):
+        options = response.css('option')
+        # The first option is -All Schools-, so we ignore it
+        # and only use [1:] of the options
+        for o in options[1:]:
+            vdc = response.meta['vdc']
+            school = School()
+            school_code = o.xpath('@value').extract()[0]
+            school_name = o.xpath('text()').extract()[0].split(' - ')[1]
+            for y in YEARS:
+                school_page_url = BASE_SHOW_URL + '?d=' + vdc['district_code'][0] + \
+                                              '&v=' + vdc['code'][0] + \
+                                              '&s=' + school_code + \
+                                              '&yr=' + str(y)
+                yield scrapy.Request(school_page_url, callback=self.parse_school_page)
+                
+    def parse_school_page(self, response):
+        school = School()
+        name_code = response.xpath('/html/body/table/tr/td/table[1]/tr/td[2]/h2/text()').extract()
+        school['name'] = name_code[0].split(': ')[1].strip() # School: School Name
+        school['code'] = name_code[1].split(': ')[1].strip() # Code: 123456789
+        school['development_region'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[2]/td[2]/text()'))
+        school['eco_belt'] =  val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[3]/td[2]/text()'))
+        school['zone'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[5]/td[2]/text()'))
+        school['district'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[5]/td[2]/text()'))
+        school['vdc'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[6]/td[2]/text()'))
+        school['address'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[7]/td[2]/text()'))
+        school['ward_no'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[8]/td[2]/text()'))
+        school['account_no'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[9]/td[2]/text()'))
+        school['locality'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[10]/td[2]/text()'))
+        school['phone'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[11]/td[2]/text()'))
+        school['email'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[12]/td[2]/text()'))
+        school['resource_center'] = val(response.xpath('/html/body/table/tr/td/table[2]/tr/td[1]/table/tr[13]/td[2]/text()'))
+        yield school
